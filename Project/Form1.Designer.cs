40e360d53d095391a8059a01caa4946ba34b86f7
has-sidd <59255860+has-sidd@users.noreply.github.com>
2021-08-08 17:19:19 +0500

    Add files via upload

---
+++ Project/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Project
+{
+    partial class crash2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.traffic1 = new System.Windows.Forms.PictureBox();
+            this.traffic2 = new System.Windows.Forms.PictureBox();
+            this.traffic3 = new System.Windows.Forms.PictureBox();
+            this.traffic4 = new System.Windows.Forms.PictureBox();
+            this.traffic5 = new System.Windows.Forms.PictureBox();
+            this.traffic6 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.crash = new System.Windows.Forms.Label();
+            this.crash1 = new System.Windows.Forms.Label();
+            this.over = new System.Windows.Forms.Label();
+            this.winner = new System.Windows.Forms.Label();
+            this.traffic7 = new System.Windows.Forms.PictureBox();
+            this.traffic8 = new System.Windows.Forms.PictureBox();
+            this.name = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(185, 439);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 80);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(554, 439);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(55, 80);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // traffic1
+            // 
+            this.traffic1.Location = new System.Drawing.Point(50, 389);
+            this.traffic1.Name = "traffic1";
+            this.traffic1.Size = new System.Drawing.Size(55, 80);
+            this.traffic1.TabIndex = 2;
+            this.traffic1.TabStop = false;
+            // 
+            // traffic2
+            // 
+            this.traffic2.Location = new System.Drawing.Point(162, 132);
+            this.traffic2.Name = "traffic2";
+            this.traffic2.Size = new System.Drawing.Size(55, 80);
+            this.traffic2.TabIndex = 2;
+            this.traffic2.TabStop = false;
+            // 
+            // traffic3
+            // 
+            this.traffic3.Location = new System.Drawing.Point(283, 290);
+            this.traffic3.Name = "traffic3";
+            this.traffic3.Size = new System.Drawing.Size(55, 80);
+            this.traffic3.TabIndex = 2;
+            this.traffic3.TabStop = false;
+            // 
+            // traffic4
+            // 
+            this.traffic4.Location = new System.Drawing.Point(416, 389);
+            this.traffic4.Name = "traffic4";
+            this.traffic4.Size = new System.Drawing.Size(55, 80);
+            this.traffic4.TabIndex = 2;
+            this.traffic4.TabStop = false;
+            // 
+            // traffic5
+            // 
+            this.traffic5.Location = new System.Drawing.Point(524, 132);
+            this.traffic5.Name = "traffic5";
+            this.traffic5.Size = new System.Drawing.Size(55, 80);
+            this.traffic5.TabIndex = 2;
+            this.traffic5.TabStop = false;
+            // 
+            // traffic6
+            // 
+            this.traffic6.Location = new System.Drawing.Point(633, 290);
+            this.traffic6.Name = "traffic6";
+            this.traffic6.Size = new System.Drawing.Size(55, 80);
+            this.traffic6.TabIndex = 2;
+            this.traffic6.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(38, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Car 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(38, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Car 2";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // crash
+            // 
+            this.crash.AutoSize = true;
+            this.crash.BackColor = System.Drawing.Color.Transparent;
+            this.crash.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.crash.ForeColor = System.Drawing.Color.DarkRed;
+            this.crash.Location = new System.Drawing.Point(88, 162);
+            this.crash.Name = "crash";
+            this.crash.Size = new System.Drawing.Size(240, 31);
+            this.crash.TabIndex = 9;
+            this.crash.Text = "Car 1 CRASHED!";
+            // 
+            // crash1
+            // 
+            this.crash1.AutoSize = true;
+            this.crash1.BackColor = System.Drawing.Color.Transparent;
+            this.crash1.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.crash1.ForeColor = System.Drawing.Color.DarkRed;
+            this.crash1.Location = new System.Drawing.Point(452, 162);
+            this.crash1.Name = "crash1";
+            this.crash1.Size = new System.Drawing.Size(240, 31);
+            this.crash1.TabIndex = 10;
+            this.crash1.Text = "Car 2 CRASHED!";
+            // 
+            // over
+            // 
+            this.over.AutoSize = true;
+            this.over.BackColor = System.Drawing.Color.Transparent;
+            this.over.Font = new System.Drawing.Font("Modern No. 20", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.over.ForeColor = System.Drawing.Color.DarkRed;
+            this.over.Location = new System.Drawing.Point(221, 226);
+            this.over.Name = "over";
+            this.over.Size = new System.Drawing.Size(337, 65);
+            this.over.TabIndex = 11;
+            this.over.Text = "Game Over!";
+            // 
+            // winner
+            // 
+            this.winner.AutoSize = true;
+            this.winner.BackColor = System.Drawing.Color.Transparent;
+            this.winner.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winner.ForeColor = System.Drawing.Color.DarkRed;
+            this.winner.Location = new System.Drawing.Point(256, 307);
+            this.winner.Name = "winner";
+            this.winner.Size = new System.Drawing.Size(135, 38);
+            this.winner.TabIndex = 12;
+            this.winner.Text = "Winner";
+            // 
+            // traffic7
+            // 
+            this.traffic7.Location = new System.Drawing.Point(283, 12);
+            this.traffic7.Name = "traffic7";
+            this.traffic7.Size = new System.Drawing.Size(55, 80);
+            this.traffic7.TabIndex = 2;
+            this.traffic7.TabStop = false;
+            // 
+            // traffic8
+            // 
+            this.traffic8.Location = new System.Drawing.Point(633, 12);
+            this.traffic8.Name = "traffic8";
+            this.traffic8.Size = new System.Drawing.Size(55, 80);
+            this.traffic8.TabIndex = 2;
+            this.traffic8.TabStop = false;
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.BackColor = System.Drawing.Color.Transparent;
+            this.name.Font = new System.Drawing.Font("Modern No. 20", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.ForeColor = System.Drawing.Color.DarkRed;
+            this.name.Location = new System.Drawing.Point(137, 214);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(557, 65);
+            this.name.TabIndex = 15;
+            this.name.Text = "Top Speed Triumph";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(736, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 59);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(736, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 59);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // crash2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 611);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.winner);
+            this.Controls.Add(this.over);
+            this.Controls.Add(this.crash1);
+            this.Controls.Add(this.crash);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.traffic7);
+            this.Controls.Add(this.traffic6);
+            this.Controls.Add(this.traffic8);
+            this.Controls.Add(this.traffic5);
+            this.Controls.Add(this.traffic4);
+            this.Controls.Add(this.traffic3);
+            this.Controls.Add(this.traffic2);
+            this.Controls.Add(this.traffic1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "crash2";
+            this.Text = "Top Speed Triumph";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traffic8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox traffic1;
+        private System.Windows.Forms.PictureBox traffic2;
+        private System.Windows.Forms.PictureBox traffic3;
+        private System.Windows.Forms.PictureBox traffic4;
+        private System.Windows.Forms.PictureBox traffic5;
+        private System.Windows.Forms.PictureBox traffic6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label crash;
+        private System.Windows.Forms.Label crash1;
+        private System.Windows.Forms.Label over;
+        private System.Windows.Forms.Label winner;
+        private System.Windows.Forms.PictureBox traffic7;
+        private System.Windows.Forms.PictureBox traffic8;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Project/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Project
+{
+    public partial class crash2 : Form
+    {
+        public crash2()
+        {
+            InitializeComponent();
+            //Labels used:
+            crash.Visible = false;
+            crash1.Visible = false;
+            over.Visible = false;
+            winner.Visible = false;
+        }
+        int carspeed = 15;  //change in cars
+        double trafficspeed1, trafficspeed2; // speed of the game(difficulty)
+        int score1, score2 = 0;
+        // Sounds used:
+        SoundPlayer crash_sound = new SoundPlayer(@"E:/crash.wav");
+        SoundPlayer carstart = new SoundPlayer(@"E:/carstart.wav");
+        SoundPlayer carmoving = new SoundPlayer(@"E:/carmoving.wav");
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            this.BackgroundImage = Image.FromFile(@"E:/Background2.png");  //BackGround Tracks
+            carstart.Play(); //sound of car starting when form is loaded
+
+            button1.Text = "START";
+            button1.ForeColor = Color.Yellow;
+            button1.BackColor = Color.Red;
+
+            button2.Text = "EXIT";
+            button2.ForeColor = Color.Yellow;
+            button2.BackColor = Color.Red;
+
+            Image car1 = Image.FromFile(@"E:/car.png");                   //Car 1 image
+            pictureBox1.Image = car1;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Location = new Point(165, 500);
+
+            Image car2 = Image.FromFile(@"E:/car1.png");                   //Car 2 image
+            pictureBox2.Image = car2;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Location = new Point(525, 500);
+
+            Image t1 = Image.FromFile(@"E:/traffic1.png");                   //traffic 1 image
+            traffic1.Image = t1;
+            traffic1.SizeMode = PictureBoxSizeMode.StretchImage;
+            traffic1.BackColor = Color.Transparent;
+
+            Image t2 = Image.FromFile(@"E:/traffic2.png");                   //traffic 2 image
+            traffic2.Image = t2;
+            traffic2.SizeMode = PictureBoxSizeMode.StretchImage;
+            traffic2.BackColor = Color.Transparent;
+
+            Image t3 = Image.FromFile(@"E:/traffic3.png");                   //traffic 3 image
+            traffic3.Image = t3;
+            traffic3.SizeMode = PictureBoxSizeMode.StretchImage;
+            traffic3.BackColor = Color.Transparent;
+
+            Image t7 = Image.FromFile(@"E:/traffic6.png");                   //traffic 7 image
+            traffic7.Image = t7;
+            traffic7.SizeMode = PictureBoxSizeMode.StretchImage;
+            traffic7.BackColor = Color.Transparent;
+
+            Image t4 = Image.FromFile(@"E:/traffic4.png");                   //traffic 4 image
+            traffic4.Image = t4;
+            traffic4.SizeMode = PictureBoxSizeMode.StretchImage;
+            traffic4.BackColor = Color.Transparent;
+
+            Image t5 = Image.FromFile(@"E:/traffic5.png");                   //traffic 5 image
+            traffic5.Image = t5;
+            traffic5.SizeMode = PictureBoxSizeMode.StretchImage;
+            traffic5.BackColor = Color.Transparent;
+
+            Image t6 = Image.FromFile(@"E:/traffic6.png");                   //traffic 6 image
+            traffic6.Image = t6;
+            traffic6.SizeMode = PictureBoxSizeMode.StretchImage;
+            traffic6.BackColor = Color.Transparent;
+
+            Image t8 = Image.FromFile(@"E:/traffic1.png");                   //traffic 8 image
+            traffic8.Image = t8;
+            traffic8.SizeMode = PictureBoxSizeMode.StretchImage;
+            traffic8.BackColor = Color.Transparent;
+
+           
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            //Car 2 Controls>
+            if ( e.KeyCode == Keys.Left)
+            {
+                if ( pictureBox2.Left > 405)
+                {
+                    pictureBox2.Left -= carspeed;
+                }               
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if ( pictureBox2.Right < 720)
+                {
+                    pictureBox2.Left += carspeed;
+                }                 
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (pictureBox2.Top > 0)
+                {
+                    pictureBox2.Top -= carspeed;
+                }
+                    
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (pictureBox2.Top < 500)
+                {
+                    pictureBox2.Top += carspeed;
+                }
+            }
+            //Car 1 Controls(W,A,S,D):
+            if (e.KeyCode == Keys.A)
+            {
+                if (pictureBox1.Left > 40)
+                {
+                    pictureBox1.Left -= carspeed;
+                }
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                if (pictureBox1.Right < 370)
+                {
+                    pictureBox1.Left += carspeed;
+                }
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                if (pictureBox1.Top > 0)
+                {
+                    pictureBox1.Top -= carspeed;
+                }
+                e.SuppressKeyPress = true;
+
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                if (pictureBox1.Top < 500)
+                {
+                    pictureBox1.Top += carspeed;
+                }
+                e.SuppressKeyPress = true;
+            }
+        }
+        void game()
+        {
+            if (timer1.Enabled==false && timer2.Enabled==false)
+            {
+                if (score1 > score2)
+                { winner.Text = "Winner is Car 1"; }
+                else { winner.Text = "Winner is Car 2"; }
+                carmoving.Stop();        //Sound stops
+                over.Show();            //Game over label
+                winner.Show();         //Winner label 
+                button2.Enabled = true;
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            carmoving.PlayLooping();      // car moving sound
+            timer1.Start();
+            timer2.Start();
+            name.Hide();
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            trafficspeed1 += 0.1;   //speed icreases on every tick
+            score1 += 1;            //score also increases
+            label1.Text = "Car 1:" + score1.ToString();
+
+            Random r1 = new Random();
+            int x = r1.Next(40, 340);  //for car1
+            int y = r1.Next(0, 0);   //so traffic always start coming from top
+
+            if (traffic1.Top >= 500)
+            { traffic1.Location = new Point(x, y); }
+            else { traffic1.Top += Convert.ToInt32(trafficspeed1); }
+            if (traffic2.Top >= 500)
+            { traffic2.Location = new Point(x, y); }
+            else{ traffic2.Top += Convert.ToInt32(trafficspeed1); }
+            if (traffic3.Top >= 500)
+            { traffic3.Location = new Point(x, y); }
+            else{ traffic3.Top += Convert.ToInt32(trafficspeed1); }
+            if (traffic7.Top >= 500)
+            { traffic7.Location = new Point(x, y); }
+            else { traffic7.Top += Convert.ToInt32(trafficspeed1); }
+
+            if (pictureBox1.Bounds.IntersectsWith(traffic1.Bounds))   //crash
+            {
+                timer1.Enabled = false;
+                crash.Show();    //crash label
+                
+            }
+            if (pictureBox1.Bounds.IntersectsWith(traffic2.Bounds))
+            {
+                timer1.Enabled = false;
+                crash.Show();
+                
+            }
+            if (pictureBox1.Bounds.IntersectsWith(traffic3.Bounds))
+            {
+                timer1.Enabled = false;
+                crash.Show();
+                
+            }
+            if (pictureBox1.Bounds.IntersectsWith(traffic7.Bounds))
+            {
+                timer1.Enabled = false;
+                crash.Show();
+                
+            }
+            game();   //game over function called
+        }
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            trafficspeed2 += 0.1;
+            score2 += 1;
+            label2.Text = "Car 2:" + score2.ToString();
+
+            Random r2 = new Random();    //car2
+            int x = r2.Next(405, 690);
+            int y = r2.Next(0, 0);
+
+            if (traffic4.Top >= 500)
+            { traffic4.Location = new Point(x, y); ; }
+            else { traffic4.Top += Convert.ToInt32(trafficspeed2); }  //double need to be converted to string
+            if (traffic5.Top >= 500)
+            { traffic5.Location = new Point(x, y); }
+            else { traffic5.Top += Convert.ToInt32(trafficspeed2); }
+            if (traffic6.Top >= 500)
+            { traffic6.Location = new Point(x, y); ; }
+            else { traffic6.Top += Convert.ToInt32(trafficspeed2); }
+            if (traffic8.Top >= 500)
+            { traffic8.Location = new Point(x, y); ; }
+            else { traffic8.Top += Convert.ToInt32(trafficspeed2); }
+
+            if (pictureBox2.Bounds.IntersectsWith(traffic4.Bounds))     //crash
+            {  
+                timer2.Enabled = false;
+                crash1.Show();
+            }
+            if (pictureBox2.Bounds.IntersectsWith(traffic5.Bounds))
+            {
+                timer2.Enabled = false;
+                crash1.Show();
+            }
+            if (pictureBox2.Bounds.IntersectsWith(traffic6.Bounds))
+            {
+                timer2.Enabled = false;
+                crash1.Show();
+            }
+            if (pictureBox2.Bounds.IntersectsWith(traffic8.Bounds))
+            {
+                timer2.Enabled = false;
+                crash1.Show();
+            }
+            game();
+        }
+        
+       
+    }
+}
